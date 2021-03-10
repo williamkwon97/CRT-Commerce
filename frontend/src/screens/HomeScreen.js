ca4d9d076ab414d57a199886b5709039b72becdc
Transadmin <transadmin@williamkwon.local>
2021-03-09 23:14:22 -0600

    6 11:11

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Row, Col}  from "react-bootstrap"
+/*
+function HomeScreen() {
+    return (
+        <div>
+            <h1>Latest Product</h1>
+            <Row>
+                {product.map(product => (
+                    <Col sm={12} md={6} Lg={4} xL={3}>  
+                    <h3>{product.name}</h3>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    )
+}
+
+export default HomeScreen
+*/
