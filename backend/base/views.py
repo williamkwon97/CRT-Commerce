50be428818e262ea882e5008f8176253003fe799
Transadmin <transadmin@williamkwon.local>
2021-04-18 23:08:31 -0500

    yser serializer

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,8 +4,12 @@ from .products import products
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
 # Create your views here.
+
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -16,11 +20,31 @@ def getRoutes(request):
     ]
     return Response(routes)
 @api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+  
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
 
+        # Add custom claims
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        # ...
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
