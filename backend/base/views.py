3bc48a7e27dbf2edc07645849ee16759e8dae314
Transadmin <transadmin@williamkwon.local>
2021-04-22 00:10:30 -0500

    36 Register
    User

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,49 +1,71 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+
 from .models import Product
-from .serializers import ProductSerializer, UserSerializer , UserSerializerwithToken
+from .serializers import ProductSerializer, UserSerializer, UserSerializerwithToken
+
 # Create your views here.
 
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
 @api_view(['GET'])
 def getRoutes(request):
-    routes =[
+    routes = [
         "/api/products/",
-        "/api/products/create/", 
+        "/api/products/create/",
         "/api/products/upload/"
     ]
     return Response(routes)
+
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
-  
+
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    
+
     def validate(self, attrs):
-        
+
         data = super().validate(attrs)
 
-        serializer = UserSerializerwithToken(self.user).data 
-        for k, v in serializer.items():    
-            data[k] = v 
+        serializer = UserSerializerwithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
         return data
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
