ed97b834176174abf5cdf4fd908dc4a861269f1f
Transadmin <transadmin@williamkwon.local>
2021-05-16 19:15:16 -0500

    userinfo

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,10 +5,14 @@ import {  } from 'module'
 import { productListReducer, productDetailsReducer} from './reducers/productReducers'
 import {  cartReducer} from './reducers/cartReducers'
 
+import { userLoginReducer } from './reducers/userReducer';
+
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails : productDetailsReducer,
     cart : cartReducer,
+    userLogin: userLoginReducer,
+
 })
 
 
@@ -16,8 +20,13 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')) : []
 
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart:{cartItems: cartItemsFromStorage}
+    cart:{cartItems: cartItemsFromStorage},
+    userLogin: {userInfo: userInfoFromStorage}
+
 }
 
 const middleware = [thunk]
