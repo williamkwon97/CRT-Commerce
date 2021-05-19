ad803cf7ef0cc102e156e2c5a93c3d3ddda0dbcd
Transadmin <transadmin@williamkwon.local>
2021-05-18 22:26:26 -0500

    userinfo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 /*import HomeScreen from './screens/HomeScreen' */
@@ -17,6 +18,7 @@ function App() {
     <main className='py-3'>
       <Container>
         <Route path="/" exact component={HomeScreen} />
+        <Route path="/login" exact component={LoginScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
       </Container>
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { Row, Col, Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector} from 'react-redux';
+import { login } from '../actions/userActions'
+import FormContainer from '../components/FormContainer'
+
 
 function LoginScreen() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submitHandler')
+    }
+    
     return (        
-        <div>
-            
-        </div>
+        <FormContainer>
+        <h1>Sign In</h1>
+            <Form onSubmit={submitHandler}>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder= 'Enter Email'
+                        value = {email}
+                        onChange={(e) => setEmail(e.target.value)}
+
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder= 'Enter Password'
+                        value = {password}
+                        onChange={(e) => setPassword(e.target.value)}
+
+                    ></Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant='primary'> Sign In </Button>
+
+            </Form>
+        </FormContainer>
     )
 }
 
